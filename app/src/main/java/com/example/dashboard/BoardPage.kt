2e8c2582dc24b8fc4184bfd8ba03da08fc519429
Onkar <Onkarsarna@gmail.com>
2023-02-22 13:58:10 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dashboard/BoardPage.kt
@@ -0,0 +1,11 @@
+package com.example.dashboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BoardPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_board_page)
+    }
+}
---
+++ app/src/main/java/com/example/dashboard/Login.kt
@@ -0,0 +1,58 @@
+package com.example.dashboard
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        supportActionBar?.hide()
+        this.getWindow().setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+
+     private fun dataCheck() {
+          if (loginInput.editableText.toString().isEmpty())  {
+              Toast.makeText(this,"Please Enter Email Id",Toast.LENGTH_SHORT).show()
+              return
+          }
+         else if(pswdInput.editableText.toString().isEmpty()) {
+             Toast.makeText(this,"Please Enter Password",Toast.LENGTH_SHORT).show()
+         return
+         }
+    }
+
+   private fun credCheck(){
+        val userName ="Demo@gmail.com"
+        val password = "Hello@123"
+        val email =loginInput.editableText.toString()
+        val pass= pswdInput.editableText.toString()
+        if(email.equals(userName, ignoreCase = true) && pass ==password )
+        {
+            val intent = Intent(this,BoardPage::class.java)
+            startActivity(intent)
+        }
+          else {
+           Toast.makeText(this, "Incorrect Credentials", Toast.LENGTH_SHORT).show()
+       }
+    }
+    fun signInBtn(view: View) {
+        /*val name = loginInput.editableText.toString()
+        Toast.makeText(this, "Button is pressed by $name", Toast.LENGTH_LONG).show()*//*
+        if (loginInput.editableText.toString().isEmpty())  {
+            Toast.makeText(this,"Please Enter Email Id",Toast.LENGTH_LONG).show()
+        }
+        else if(pswdInput.editableText.toString().isEmpty()) {
+            Toast.makeText(this,"Please Enter Password",Toast.LENGTH_LONG).show()
+        }*/
+        dataCheck()
+        credCheck()
+    }
+}
